ff1c6ecb0c0e4c3ff43c9110b4216a1dc3800e07
GabrielSchilliro <gabrielschilliro02@gmail.com>
2022-06-15 14:10:51 -0300

    ejercicios de la sesion 4 completados

--- edad.py
+++ edad.py
@@ -1,3 +1,5 @@
+# Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
 edad = int(input("¿Qué edad tenés?"))
 
 if edad >= 18:
---
+++ reverse.py
@@ -0,0 +1,6 @@
+# Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+
+range = range(1,101)
+
+for i in reversed(range):
+    print(f"{i}")
